024ce6f6ac4125fb7c5b247f01c73837da45c84b
Parth <ppmistry204@gmail.com>
2020-09-19 01:46:45 +0530

    Not working delete.

--- src/features/Details/Details.js
+++ src/features/Details/Details.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {Button, Card, Col, Form, Input, InputNumber, Modal, Row, Typography} from "antd";
+import {Button, Card, Col, Descriptions, Form, Input, InputNumber, Modal, Row, Typography} from "antd";
 import ComparisonService from "../../redux/services/ComparisonService";
-import {PlusOutlined} from '@ant-design/icons';
+import {PlusOutlined, DeleteTwoTone, DeleteOutlined} from '@ant-design/icons';
 import uid from "uid";
 
 const Details = props => {
@@ -12,20 +12,39 @@ const Details = props => {
 
   const fetchDetails = () => {
     const d = ComparisonService.getComparison(comparisonId);
+    debugger
     setDetails(d);
   };
   React.useEffect(() => {
-    setTimeout(() => fetchDetails(), 700);
+    setTimeout(() => fetchDetails(), 500);
   }, []);
   const submitForm = details => {
     ComparisonService.addDetail({ id: uid(4), ...details, comparisonId });
     setDetailModal(false);
   }
+  const deleteDetail = d => {
+    ComparisonService.deleteDetail({ ...d, comparisonId });
+    fetchDetails();
+  }
+  console.log('next Details: ', !!details);
   return <div>
     <Typography.Title level={3}>{details.title}</Typography.Title>
     <Typography.Title level={4}>({details.comparisonUnit})</Typography.Title>
     <Row>
-      {details.details.map(d => <Col span={12}><Card>{d.name}</Card></Col>)}
+      {details.details.map(d => <Col span={12} key={d.id}>
+        <Card
+          className="detail-card"
+          style={{background: `rgba(62,181,87, ${d.opacity})`}}
+          title={d.name}
+          extra={<DeleteOutlined onClick={() => deleteDetail(d)}/>}
+        >
+          <Typography.Paragraph>Cost: {d.cost}</Typography.Paragraph>
+          <Typography.Paragraph>Measure: {d.measure}</Typography.Paragraph>
+          <Typography.Paragraph>Per unit price: {d.perUnitPrice}</Typography.Paragraph>
+          <Typography.Paragraph>Buy Pref: {d.buyPref}</Typography.Paragraph>
+          <Typography.Paragraph>Opacity: {d.opacity}</Typography.Paragraph>
+        </Card>
+      </Col>)}
     </Row>
     <Button type="ghost" className="fab-button" shape="circle" icon={<PlusOutlined />} size="large" onClick={() => setDetailModal(true)} />
     <Modal
@@ -39,11 +58,11 @@ const Details = props => {
         <Form.Item name="name" label="Item Name" rules={[{required: true}]}>
           <Input autoComplete="off" />
         </Form.Item>
-        <Form.Item name="measure" label={`Measure (${details.comparisonUnit})`} rules={[{required: true}]}>
-          <Input autoComplete="off" />
-        </Form.Item>
         <Form.Item name="cost" label={`Cost (₹)`} rules={[{required: true}]}>
           <InputNumber autoComplete="off" />
+        </Form.Item>
+        <Form.Item name="measure" label={`Measure (${details.comparisonUnit})`} rules={[{required: true}]}>
+          <Input htmlType="number" autoComplete="off" />
         </Form.Item>
       </Form>
     </Modal>
--- src/redux/services/ComparisonService.js
+++ src/redux/services/ComparisonService.js
@@ -1,5 +1,9 @@
 const comparisons = [
-  {"id":"v1i", "title":"Shampoo","comparisonUnit":"Volume", details: [{id: 'a4r', name: 'H&S'}, {id: 'a4r', name: 'Dove'}]},
+  {"id":"v1i", "title":"Shampoo","comparisonUnit":"Volume",
+    details: [
+      {id: 'a4r', name: 'Dove', cost: 90, measure: 450, perUnitPrice: 0.2, buyPref: 1, opacity: 1 },
+      {id: 'g53', name: 'H&S', cost: 300, measure: 600, perUnitPrice: 0.5, buyPref: 2, opacity: 0.5},
+    ]},
   {"id":"c95", "title":"Soap","comparisonUnit":"Weight", details: []}
 ];
 
@@ -8,11 +12,37 @@ const addComparison = c => comparisons.push({ ...c, details: [] });
 const getComparison = id => comparisons.find(c => c.id === id);
 
 const getAllComparison = () => comparisons.map(c => ({...c}));
-
+const _recalculate = cmp => {
+  const sortedDetails = cmp.details.sort((a,b) => {
+    if(a.perUnitPrice > b.perUnitPrice) return 1;
+    if(a.perUnitPrice < b.perUnitPrice) return -1;
+    return 0;
+  });
+  const totalItems = sortedDetails.length;
+  const calculatedSortedDetails = sortedDetails.map((sD, index) => ({
+    ...sD,
+    buyPref: index + 1,
+    opacity: (totalItems - index) / totalItems,
+  }))
+  cmp.details = calculatedSortedDetails;
+}
 const addDetail = d => {
   const cmp = getComparison(d.comparisonId);
   if (cmp) {
+    d.perUnitPrice = parseFloat(d.cost / d.measure).toFixed(3);
     cmp.details.push(d);
+    _recalculate(cmp);
+  }
+
+}
+
+const deleteDetail = d => {
+  console.log('DEleteing: ', d);
+  debugger
+  const cmp = getComparison(d.comparisonId);
+  if (cmp) {
+    cmp.details = cmp.details.filter(newD => newD.id !== d.id);
+    _recalculate(cmp);
   }
 }
 
@@ -21,4 +51,5 @@ export default {
   getComparison,
   getAllComparison,
   addDetail,
+  deleteDetail,
 };
