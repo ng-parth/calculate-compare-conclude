50da7858e3da153774ab446d1dbaadbb622ce791
ng-parth <ppmistry204@gmail.com>
2021-04-25 02:39:50 +0530

    Added component file.

---
+++ src/features/WaMe/WaMe.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Typography, Form, Input, Button} from 'antd';
+import {Link} from "react-router-dom";
+
+const WaMe = props => {
+    const [link,setLink] = React.useState('');
+    const submitForm = ({ phoneNo }) => {
+    setLink(`https://wa.me/91${phoneNo}`);
+  };
+  return <div>
+    <Typography.Title level={2}>WhatsApp Me</Typography.Title>
+    <Link to="/">Go Back</Link>
+    <Form onFinish={submitForm} layout="vertical">
+      <Form.Item name="phoneNo" label="PhoneNo">
+        <Input addonBefore="+91" autoComplete="off" type="tel" allowClear />
+      </Form.Item>
+      {link && <Form.Item name="link" label="Link">
+        <a target="_blank" rel="noopener noreferrer"  href={link}>{link}</a>
+      </Form.Item>}
+      <Button htmlType="submit" type="primary">Get Link</Button>
+    </Form>
+  </div>
+}
+
+export default WaMe;
