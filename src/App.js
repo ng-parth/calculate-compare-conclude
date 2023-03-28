84b6d71cc4aeea10817d63eddadc1153d58b8c58
Parth <parth@halalbox.in>
2023-03-28 17:03:05 +0530

    Updated logo.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import Comparison from "./features/Home/Comparision";
 import Details from "./features/Details/Details";
 import WaMe from './features/WaMe/WaMe';
+import logo from './assets/logos/compare192.png'
 
 function App() {
   return (
     <div className="app">
+      <img src={logo} className="logo--main" alt="Logo" />
       <Router>
         <Switch>
           <Route path="/" exact component={Comparison} />
