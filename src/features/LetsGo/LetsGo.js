92df283d37c5274c95c74694672d2d9842eaf04b
Parth <parth@halalbox.in>
2024-04-10 01:28:51 +0530

    Add route facilty

--- src/features/LetsGo/LetsGo.js
+++ src/features/LetsGo/LetsGo.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import {getRoutesApi, getRouteTagsApi} from "../../redux/services/LetsGoService";
-import { Col, notification, Radio, Row, Spin, Typography} from "antd";
+import {getRoutesApi, getRouteTagsApi, postRouteApi} from "../../redux/services/LetsGoService";
+import {Col, Form, Input, Modal, notification, Radio, Row, Select, Spin, Switch, Typography} from "antd";
 import RouteWidget from "../../components/RouteWidget";
 import {PlusSquareTwoTone} from "@ant-design/icons";
 
@@ -9,6 +9,8 @@ const LetsGo = props => {
     const [loading, setLoading] = useState(true);
     const [routeTags, setRouteTags] = useState(null);
     const [tagFilter, setTagFilter] = useState(null);
+    const [showUpsertModal, setUpsertModal] = useState(false);
+    const [form] = Form.useForm();
     const getRoutes = () => {
         getRoutesApi().then(({data}) => {
             setRoutes(data.data);
@@ -31,6 +33,17 @@ const LetsGo = props => {
         getRoutes();
         getRouteTags();
     }, []);
+    const submitForm = formValues => {
+        setLoading(true);
+        postRouteApi(formValues).then(({data}) => {
+            getRoutes();
+            setUpsertModal(false);
+        }).catch(err => {
+            console.log('Err @postRouteApi: ', err);
+            notification.error({ message: 'Fail to add new route :\'('});
+            setLoading(false);
+        })
+    }
     return <div>
         <Typography.Title level="3">Routes</Typography.Title>
         <Spin spinning={loading}>
@@ -49,12 +62,47 @@ const LetsGo = props => {
                 </Col>)}
                 {!routes?.length && <Col span={8}><div>No Routes Found</div></Col>}
                 <Col span={8}><PlusSquareTwoTone style={{fontSize: 30}} onClick={() => {
-
+                    setUpsertModal(true);
                 }}/></Col>
-
             </Row>
-
         </Spin>
+        <Modal
+            title="New Route"
+            open={showUpsertModal}
+            onOk={form.submit}
+            onCancel={() => setUpsertModal(false)}
+            okText="Add New Route"
+            confirmLoading={loading}
+        >
+            <Form onFinish={submitForm} layout="vertical" form={form}>
+                <Form.Item name="busNo" label="Bus No" rules={[{required: true}]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name="routeName" label="Route Name" rules={[{required: true}]}>
+                    <Input placeholder="eg: Worli to Airoli"/>
+                </Form.Item>
+                <Form.Item name="stopName" label="Stop Name" rules={[{required: true}]}>
+                    <Input placeholder="eg: Century Bazaar"/>
+                </Form.Item>
+                <Form.Item name="defaultStopId" label="Default Stop Id" rules={[{required: true}]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name="apiUrl" label="Update API Url" rules={[{required: true}]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name="tags" label="Route Tags">
+                    <Select mode={"multiple"}>
+                        {routeTags?.map(t => <Select.Option value={t.tagName} key={t.id}>{t.tagName}</Select.Option>)}
+                    </Select>
+                </Form.Item>
+                <Form.Item name="isPublic" label="Is Public?">
+                    <Switch defaultChecked checkedChildren="YES" unCheckedChildren="NO" />;
+                </Form.Item>
+                <Form.Item name="status" label="Status?">
+                    <Switch defaultChecked checkedChildren="ACTIVE" unCheckedChildren="INACTIVE" />;
+                </Form.Item>
+            </Form>
+        </Modal>
     </div>
 }
 
--- src/redux/services/LetsGoService.js
+++ src/redux/services/LetsGoService.js
@@ -3,3 +3,4 @@ const BASE_URL = process.env.REACT_APP_ROUTE_SERVICE_BASE_URL || 'http://localho
 export const getRoutesApi = () => axios.get(`${BASE_URL}/api/letsgo/routes`);
 export const getRouteTagsApi = () => axios.get(`${BASE_URL}/api/letsgo/route-tags`);
 export const getRouteStatusApi = id => axios.get(`${BASE_URL}/api/letsgo/status/${id}`);
+export const postRouteApi = route => axios.post(`${BASE_URL}/api/letsgo/route`, route);
